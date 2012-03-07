1a3c3a756e42cf51c876f0ec88314447e7606e83
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-03-07 19:57:33 +0100

    acces fns

--- src/lan_party_manager/models/party.clj
+++ src/lan_party_manager/models/party.clj
@@ -1,8 +1,25 @@
-(ns lan-party-manager.models.party)
+(ns lan-party-manager.models.party
+  (:use somnium.congomongo))
+
+(def lan-party-required-keys #{:name})
 
 (defstruct lan-party
-  :id
-  :event_date_doodle
   :name
-  :proposed_games)
+  :doodle
+  :proposed-games)
 
+(defn id->str [obj]
+  (update-in obj [:_id] str))
+
+(defn all-lans []
+  (map id->str (fetch :lans)))
+
+(defn by-id [id]
+  (id->str (fetch-by-id :lans (object-id id))))
+
+(defn all-proposed-games []
+  (->> (fetch :lans :only [:proposed-games]) (map :proposed-games) (apply concat) (map id->str) set))
+
+(defn save-lan [{:keys [name proposed-games] :as party}]
+  ; TODO validate
+  (insert! :lans party))
