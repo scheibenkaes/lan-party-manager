61d1fbcb5095775dd28e2f447b86e4c907cb12e6
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-03-07 19:52:40 +0100

    starting db on startup

--- src/lan_party_manager/server.clj
+++ src/lan_party_manager/server.clj
@@ -1,11 +1,13 @@
 (ns lan-party-manager.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server])
+  (:require [lan-party-manager.db.connection :as db]))
 
 (server/load-views "src/lan_party_manager/views/")
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (db/init!)
     (server/start port {:mode mode
                         :ns 'lan-party-manager})))
 
